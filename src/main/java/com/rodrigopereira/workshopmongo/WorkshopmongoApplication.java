c10de7ba79053f23a0fa4efba31c5dd2cc5005a2
Rodrigo Pereira <rodrigorpogo@gmail.com>
2018-09-06 18:11:17 -0300

    Entity user e REST funcionando

--- src/main/java/com/rodrigopereira/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/rodrigopereira/workshopmongo/WorkshopmongoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopmongoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WorkshopmongoApplication.class, args);
     }
 }
