2957e3d2789faf47ce2a27048b91db4d5a218d49
Rodrigo Pereira <rodrigorpogo@gmail.com>
2018-09-11 15:16:48 -0300

    Consulta simples com '@'Query

--- src/main/java/com/rodrigopereira/workshopmongo/services/PostService.java
+++ src/main/java/com/rodrigopereira/workshopmongo/services/PostService.java
@@ -24,8 +24,12 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public List<Post> findByTitle(String text){
+    /*public List<Post> findByTitle(String text){
         return repo.findByTitleContainingIgnoreCase(text);
+    }*/
+
+    public List<Post> findByTitle(String text){
+        return repo.findByTitle(text);
     }
 
 
